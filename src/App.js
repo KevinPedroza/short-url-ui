ed94e084f66fc4609099c1cbcbb3855f594efa5b
Kevin Pedroza <kevinlarios2343@gmail.com>
2021-02-25 14:02:46 -0600

    Added files needed to routing and services

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import NavBar from './components/Navbar';
+import Top from './components/Top';
+import UrlForm from './components/UrlForm';
+import Route from './route/Route';
+
+
+const App = () => {
+    return (
+        <React.Fragment>
+            <NavBar />
+
+            <div className="ui container">
+                <Route path="/">
+                    <Top /> 
+                </Route>
+
+                <Route path="/new-url">
+                    <UrlForm />
+                </Route>
+            </div>
+        </React.Fragment>
+    );
+};
+
+export default App;
